b34702c94c49a412d5510cea09a492fcfe15fe01
ADRIEN LEBORGNE <81620287+Yasedo-03@users.noreply.github.com>
2023-07-12 17:56:54 +0200

    feat: add mainCard + scrollDoww arrow

--- racket-rivals/src/components/Homepage/MainBloc/mainBloc.component.tsx
+++ racket-rivals/src/components/Homepage/MainBloc/mainBloc.component.tsx
@@ -1,7 +1,25 @@
+import { ScrollDownArrows } from "../../ScrollArrows";
 import styles from "./MainBloc.module.scss";
 
 export const MainBloc = () => {
-  return <div className={styles.bloc}>MAIN BLOC</div>;
+  return (
+    <div className={styles.bloc}>
+      <div className={styles.mainCard}>
+        <h1 className={styles.title}>Bienvenue sur RacketRivals</h1>
+        <p className={styles.text}>
+          Votre espace pour organiser et trouver des tournois de tennis de
+          table.
+        </p>
+        <p className={styles.text}>
+          Rejoignez la compétition en quelques clics. Prêts pour le défi?
+        </p>
+        <button className={styles.loginButton} type="button">
+          Connexion
+        </button>
+      </div>
+      <ScrollDownArrows />
+    </div>
+  );
 };
 
 MainBloc.displayName = "MainBloc";
