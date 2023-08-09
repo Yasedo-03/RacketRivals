ae6ee06809ca9e7624554b9e352436638d3c0309
ADRIEN LEBORGNE <81620287+Yasedo-03@users.noreply.github.com>
2023-08-09 12:19:16 +0200

    feat: add tournament list + pagination component

--- racket-rivals/src/components/Homepage/TournamentBloc/TournamentBlocCard/TournamentBlocCard.component.tsx
+++ racket-rivals/src/components/Homepage/TournamentBloc/TournamentBlocCard/TournamentBlocCard.component.tsx
@@ -1,10 +1,14 @@
+import { Pagination } from "../../../Pagination";
 import { SearchBar } from "../../../SearchBar";
+import { TournamentList } from "../TournamentList";
 import styles from "./TournamentBlocCard.module.scss";
 
 export const TournamentBlocCard = () => {
   return (
     <div className={styles.container}>
       <SearchBar />
+      <TournamentList />
+      <Pagination />
     </div>
   );
 };
---
+++ racket-rivals/src/components/Homepage/TournamentBloc/TournamentList/TournamentList.component.tsx
@@ -0,0 +1,29 @@
+import styles from "./TournamentList.module.scss";
+
+export const TournamentList = () => {
+  const tournaments = [
+    { id: "#12345", name: "Tournoi d'ézanville" },
+    { id: "#12346", name: "Tournoi d'eaubonne" },
+    { id: "#12346", name: "Tournoi d'eaubonne" },
+    { id: "#12346", name: "Tournoi d'eaubonne" },
+    { id: "#12346", name: "Tournoi d'eaubonne" },
+    { id: "#12346", name: "Tournoi d'eaubonne" },
+    { id: "#12346", name: "Tournoi d'eaubonne" },
+    { id: "#12346", name: "Tournoi d'eaubonne" },
+    { id: "#12346", name: "Tournoi d'eaubonne" },
+    { id: "#12346", name: "Tournoi d'eaubonne" },
+  ];
+
+  return (
+    <div className={styles.list}>
+      {tournaments.map((tournament) => (
+        <a key={tournament.id} className={styles.listItem} href={tournament.id}>
+          <span>{tournament.name} </span>
+          <span>{tournament.id}</span>
+        </a>
+      ))}
+    </div>
+  );
+};
+
+TournamentList.displayName = "TournamentList";
