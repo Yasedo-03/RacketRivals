15d647662135411b5de91301d0f7c0c6d50cf20c
ADRIEN LEBORGNE <81620287+Yasedo-03@users.noreply.github.com>
2023-08-31 17:45:33 +0200

    feat: add db connection with mongoose

--- racket-rivals/server/index.js
+++ racket-rivals/server/index.js
@@ -1,6 +1,9 @@
 import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express()
 const port = 3001
@@ -8,9 +11,7 @@ const port = 3001
 app.use(express.json());
 app.use(cors());
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+mongoose.connect(process.env.MONGO_URI);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
