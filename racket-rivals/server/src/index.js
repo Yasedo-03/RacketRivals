5027bd40db6fbaef4c8ec359844fdbdd855ecdac
ADRIEN LEBORGNE <81620287+Yasedo-03@users.noreply.github.com>
2023-09-01 16:07:33 +0200

    feat: etablish db connection + auth endpoint

--- racket-rivals/server/src/index.js
+++ racket-rivals/server/src/index.js
@@ -3,6 +3,8 @@ import cors from "cors";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 
+import { userRouter } from "./routes/users.js";
+
 dotenv.config();
 
 const app = express()
@@ -11,7 +13,14 @@ const port = 3001
 app.use(express.json());
 app.use(cors());
 
-mongoose.connect(process.env.MONGO_URI);
+app.use("/auth", userRouter);
+
+mongoose.connect(process.env.MONGO_URI,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
