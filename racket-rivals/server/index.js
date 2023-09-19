e1f18cba362c86b0bc16c3e2b7513c23a73526d9
ADRIEN LEBORGNE <81620287+Yasedo-03@users.noreply.github.com>
2023-09-19 20:57:25 +0200

    fix 7

--- racket-rivals/server/index.js
+++ racket-rivals/server/index.js
@@ -24,6 +24,15 @@ app.use(express.json());
 // app.use(cors(corsOptions));
 app.use(cookieParser());
 
+
+const corsConfig = {
+  origin: '',
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE']
+}
+app.use(cors(corsConfig))
+app.options("", cors(corsConfig))
+
 app.use("/", (req, res) => {
   res.send("Server is running");
 })
