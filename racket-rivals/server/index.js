47575861f52f40116bf3915385a3bc244cf92f28
ADRIEN LEBORGNE <81620287+Yasedo-03@users.noreply.github.com>
2023-09-20 18:29:39 +0200

    fix vercel 17

--- racket-rivals/server/index.js
+++ racket-rivals/server/index.js
@@ -20,12 +20,6 @@ const port = process.env.PORT || 5001;
 //   credentials: true,
 // };
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
-
 app.use(express.json());
 // app.use(cors(corsOptions));
 app.use(cookieParser());
