c1c58ff88f1b9d42ade40d92b1ab81db11e3f068
ADRIEN LEBORGNE <81620287+Yasedo-03@users.noreply.github.com>
2023-09-23 23:37:33 +0200

    fix vercel 2

---
+++ racket-rivals/server/api/users/users.js
@@ -0,0 +1,13 @@
+import { getUsers } from "../../src/controllers/usersControllers.js";
+
+export default async (req, res) => {
+  if (req.method === 'GET') {
+    try {
+      await getUsers(req, res);
+    } catch (error) {
+      res.status(500).json({ error: 'Une erreur est survenue' });
+    }
+  } else {
+    res.status(405).end();
+  }
+};
